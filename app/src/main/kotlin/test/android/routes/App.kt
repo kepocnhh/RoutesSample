49eb316cb14913b6dd67242bc504330c36eac79a
Stanley Wintergreen <kepocnhh@protonmail.com>
2026-02-13 23:23:08 +0300

    init android

---
+++ app/src/main/kotlin/test/android/routes/App.kt
@@ -0,0 +1,10 @@
+package test.android.routes
+
+import android.app.Application
+
+internal class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        // todo
+    }
+}
