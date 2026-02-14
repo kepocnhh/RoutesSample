a3ef58c9a7cf3f0177f98963dc3aa84742048577
Stanley Wintergreen <kepocnhh@protonmail.com>
2026-02-14 15:01:01 +0300

    animatable

--- app/src/main/kotlin/test/android/routes/MainScreen.kt
+++ app/src/main/kotlin/test/android/routes/MainScreen.kt
@@ -2,6 +2,11 @@ package test.android.routes
 
 import androidx.activity.compose.BackHandler
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.VectorConverter
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateOffsetAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
@@ -11,13 +16,20 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalWindowInfo
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -119,10 +131,21 @@ internal fun BarScreen(onBack: () -> Unit) {
 internal fun V0Screen(onBack: () -> Unit) {
     val routes = App.routes()
     val screens = routes.screens.collectAsState().value
+    val size = LocalWindowInfo.current.containerSize
+    val animatable = remember { Animatable(size.width.dp, Dp.VectorConverter, null, "v0") }
+    LaunchedEffect(screens.size) {
+        val value = if ("v0" == screens.lastOrNull()) 0.dp else size.width.dp
+        animatable.animateTo(value, tween())
+    }
+    val x = animatable.asState().value
+    LaunchedEffect(x) {
+        println("x: $x")
+    }
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.White)
+            .offset(x = x)
+            .background(Color.Red)
             .clickable(
                 interactionSource = null,
                 indication = null,
@@ -159,13 +182,14 @@ internal fun V0Screen(onBack: () -> Unit) {
                 text = "back",
             )
         }
-        AnimatedVisibility(
-            visible = screens.contains("v1"),
-            enter = fadeIn(),
-            exit = fadeOut(),
-        ) {
-            V1Screen(onBack = routes::back)
-        }
+    }
+    AnimatedVisibility(
+        modifier = Modifier.fillMaxSize(),
+        visible = screens.contains("v1"),
+        enter = fadeIn(),
+        exit = fadeOut(),
+    ) {
+        V1Screen(onBack = routes::back)
     }
 }
 
