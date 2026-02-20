df56ed3ecf70dda0efcf604882282c53fed8856c
Stanley Wintergreen <kepocnhh@protonmail.com>
2026-02-20 16:33:57 +0300

    detail

--- app/src/main/kotlin/test/android/routes/FooListScreen.kt
+++ app/src/main/kotlin/test/android/routes/FooListScreen.kt
@@ -3,14 +3,18 @@ package test.android.routes
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -18,9 +22,12 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.withContext
 import test.android.routes.entity.Foo
+import java.util.UUID
 
 @Composable
 internal fun FooListScreen(
@@ -30,11 +37,19 @@ internal fun FooListScreen(
     RoutesAnimations(
         modifier = modifier,
         route = "foo:list",
-        content = {
-            FooListScreen(onBack = onBack)
+        content = { routes ->
+            FooListScreen(
+                onBack = onBack,
+                toObject = { id ->
+                    routes.next("foo:detail")
+                },
+            )
         },
-        foreground = {
-            // todo
+        foreground = { routes ->
+            FooDetailScreen(
+                modifier = Modifier.fillMaxSize(),
+                onBack = routes::back,
+            )
         },
     )
 }
@@ -42,6 +57,7 @@ internal fun FooListScreen(
 @Composable
 internal fun FooListScreen(
     onBack: () -> Unit,
+    toObject: (UUID) -> Unit,
 ) {
     val routes: Routes = LocalRoutes.current
     val state = routes.states.collectAsState().value
@@ -62,12 +78,24 @@ internal fun FooListScreen(
         }
         LazyColumn(
             modifier = Modifier.fillMaxSize(),
-            contentPadding = insets,
+            contentPadding = PaddingValues(
+                top = insets.calculateTopPadding() + 8.dp,
+                bottom = insets.calculateBottomPadding() + 8.dp,
+            ),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
         ) {
             objectsState.value.forEach { obj ->
                 item(key = obj.id) {
                     Column(
-                        modifier = Modifier.fillMaxWidth(),
+                        modifier = Modifier
+                            .padding(horizontal = 16.dp)
+                            .fillMaxWidth()
+                            .background(color = Color.LightGray, shape = RoundedCornerShape(16.dp))
+                            .clip(RoundedCornerShape(16.dp))
+                            .clickable {
+                                toObject(obj.id)
+                            }
+                            .padding(horizontal = 16.dp, vertical = 8.dp),
                     ) {
                         BasicText(
                             modifier = Modifier.fillMaxWidth(),
